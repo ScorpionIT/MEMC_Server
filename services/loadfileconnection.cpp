2b334870fa93b2cb1dea621b7bcd5c4405c277cc
Elia Devito <eliadevito@yahoo.it>
2015-01-10 19:00:41 +0100

    Fixed fileupload

--- services/loadfileconnection.cpp
+++ services/loadfileconnection.cpp
@@ -8,6 +8,8 @@ using namespace services;
 LoadFileConnection::LoadFileConnection( QTcpSocket* client )
 {
     this->client = client;
+    this->client->setParent( nullptr );
+    this->client->moveToThread( this );
 }
 
 void LoadFileConnection::closeConnection()
@@ -31,7 +33,7 @@ void LoadFileConnection::run()
 
     UserManager* userManager = UserManager::getInstance();
 
-    userName_ID[1].chop( 2 );
+    userName_ID[1].chop ( 1 );
 
     if( !userManager->getUser( userName_ID[0] )->isOnLine( userName_ID[1].toInt() ) )
     {
@@ -41,86 +43,97 @@ void LoadFileConnection::run()
     }
     else
     {
-        User* user = userManager->getUser( userName_ID[0] );
-
-        this->client->write( "what do you have for me?[MUSIC=1, VIDEO=2, IMAGES=3]\n" );
-        this->client->waitForBytesWritten( -1 );
-
-        this->client->waitForReadyRead();
-        QString answer = this->client->readLine();
-
-
-        QString directory = user->getUserDirectory();
-
-        int choice = answer.toInt();
-
-        switch ( choice )
-        {
-        case 1:
-            directory += QString( "Music" ) + QString( "/" );
-            break;
-
-        case 2:
-            directory += QString( "Video" ) + QString( "/" );
-            break;
-        case 3:
-            directory += QString( "Images" ) + QString( "/" );
-            break;
-        default:
-            this->closeConnection();
-            return;
-        }
-
-        qDebug() << directory;
-
-        this->client->write( "name?\n" );
-        this->client->waitForBytesWritten( -1 );
-
-        this->client->waitForReadyRead();
-        QString fileName = this->client->readLine();
-        fileName.chop( 1 );
-
-        QFile* file = new QFile( directory + fileName );
-
-        this->client->write( "buffer length?\n" );
-        this->client->waitForBytesWritten( -1 );
-
-        this->client->waitForReadyRead();
-        int bufferLength = QString( this->client->readLine() ).toInt();
-
-        file->open( QIODevice::WriteOnly );
-        QTextStream fileStream( file );
-
-        int filePieces = 1;
-
-        bool isTrasmitting = this->client->waitForReadyRead();
-        QString buffer = this->client->read( bufferLength );
-
-        while( buffer != "END\n" )
-        {
-            fileStream << buffer;
-
-            this->client->write( QString::number( filePieces ).toUtf8() + "\n" );
-            this->client->waitForBytesWritten( -1 );
-
-            filePieces++;
-
-            isTrasmitting = this->client->waitForReadyRead();
-            buffer = this->client->read( bufferLength );
-
-            if( !isTrasmitting )
-            {
-                file->remove();
-                delete file;
-                this->closeConnection();
-            }
-        }
-
-        file->close();
-
         this->client->write( "ok\n" );
         this->client->waitForBytesWritten( -1 );
+        User* user = userManager->getUser( userName_ID[0] );
 
+        while (true)
+        {
+            this->client->write( "what do you have for me?[MUSIC=1, VIDEO=2, IMAGES=3, FINISH=4]\n" );
+            this->client->waitForBytesWritten( -1 );
+
+            this->client->waitForReadyRead();
+            QString answer = this->client->readLine();
+
+            if (answer == "4\n")
+            {
+                qDebug() << "END FILE LIST";
+                this->client->close();
+                return;
+            }
+
+            QString directory = user->getUserDirectory();
+
+            int choice = answer.toInt();
+
+            switch ( choice )
+            {
+            case 1:
+                directory += QString( "Music" ) + QString( "/" );
+                break;
+
+            case 2:
+                directory += QString( "Videos" ) + QString( "/" );
+                break;
+            case 3:
+                directory += QString( "Images" ) + QString( "/" );
+                break;
+            default:
+                this->closeConnection();
+                return;
+            }
+
+            qDebug() << directory;
+
+            this->client->write( "name?\n" );
+            this->client->waitForBytesWritten( -1 );
+
+            this->client->waitForReadyRead();
+            QString fileName = this->client->readLine();
+            fileName.chop( 1 );
+
+            QFile* file = new QFile( directory + fileName );
+
+            this->client->write( "buffer length?\n" );
+            this->client->waitForBytesWritten( -1 );
+
+            this->client->waitForReadyRead();
+            int bufferLength = QString( this->client->readLine() ).toInt();
+
+            file->open( QIODevice::WriteOnly );
+
+            int filePieces = 1;
+
+            bool isTrasmitting = this->client->waitForReadyRead();
+            QByteArray buffer = this->client->read( bufferLength );
+
+            while( QString( buffer ) != "END\n" && QString( buffer ) != "STOP\n" )
+            {
+                file->write( buffer );
+                this->client->write( QString::number( filePieces ).toUtf8() + "\n" );
+                this->client->waitForBytesWritten( -1 );
+
+                filePieces++;
+
+                isTrasmitting = this->client->waitForReadyRead();
+                buffer = this->client->read( bufferLength );
+
+                if( !isTrasmitting )
+                {
+                    file->remove();
+                    delete file;
+                    this->closeConnection();
+                }
+            }
+            if (QString( buffer ) == "STOP\n")
+                file->remove();
+
+            file->close();
+            delete file;
+
+            this->client->write( "ok\n" );
+            this->client->waitForBytesWritten( -1 );
+        }
     }
 }
 
